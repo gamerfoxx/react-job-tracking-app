b82bbaf9516f1006434d71a2a4eba4f1feb99600
gamerfoxx <admin@josephludbrook.com>
2023-08-22 15:07:32 -0400

    complete error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,16 +1,31 @@
 import { Link, useRouteError } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/ErrorPage';
+import img from '../assets/images/not-found.svg';
 
 const ErrorPage = () => {
 	const error = useRouteError();
 	console.log(error);
+	if (error.status === 404) {
+		return (
+			<Wrapper>
+				<div>
+					<img src={img} />
+					<p>Page Not found!</p>
+					<p>
+						<b>{error.status}</b> {error.data}
+					</p>
+					<Link to="/">Home</Link>
+				</div>
+			</Wrapper>
+		);
+	}
 	return (
-		<div>
-			<h1>ErrorPage</h1>
+		<Wrapper>
 			<p>
 				<b>{error.status}</b> {error.data}
 			</p>
 			<Link to="/">Home</Link>
-		</div>
+		</Wrapper>
 	);
 };
 export default ErrorPage;
