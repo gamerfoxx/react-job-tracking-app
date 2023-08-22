f00aa921ff3eceb8b32380079f106902df3ed6d6
gamerfoxx <admin@josephludbrook.com>
2023-08-22 14:58:37 -0400

    complete landing page

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -2,11 +2,10 @@ import { Outlet } from 'react-router-dom';
 
 const HomeLayout = () => {
 	return (
-		<div>
-			<nav>Navbar</nav>
+		<>
 			<Outlet />
 			{/* Outlet used to dynamically display child pages through navigation */}
-		</div>
+		</>
 	);
 };
 export default HomeLayout;
