98224808dc44a7717bec76f5cdced62f116baf75
gamerfoxx <admin@josephludbrook.com>
2023-08-22 14:36:40 -0400

    configure error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,4 +1,16 @@
+import { Link, useRouteError } from 'react-router-dom';
+
 const ErrorPage = () => {
-	return <h1>ErrorPage</h1>;
+	const error = useRouteError();
+	console.log(error);
+	return (
+		<div>
+			<h1>ErrorPage</h1>
+			<p>
+				<b>{error.status}</b> {error.data}
+			</p>
+			<Link to="/">Home</Link>
+		</div>
+	);
 };
 export default ErrorPage;
