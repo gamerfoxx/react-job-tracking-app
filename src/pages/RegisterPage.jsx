662dc143d1a53f14f7d835507dac1f664d256e2c
gamerfoxx <admin@josephludbrook.com>
2023-08-22 16:40:43 -0400

    complete base of register form

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,12 +1,44 @@
 import { Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { Logo } from '../components';
 
 const RegisterPage = () => {
 	return (
-		<div>
-			<h1>RegisterPage</h1>
-			<Link to="/login">Login Page</Link>
-			{/* Must use a tag to create links to external sites */}
-		</div>
+		<Wrapper>
+			<form className="form">
+				<Logo />
+				<div className="form-row">
+					<h4> Register</h4>
+					<label
+						htmlFor="userName"
+						className="form-label">
+						Name
+					</label>
+					<input
+						type="text"
+						id="userName"
+						name="userName"
+						className="form-input"
+						defaultValue="Joey"
+						required
+					/>
+				</div>
+				<button
+					type="submit"
+					className="btn btn-block">
+					Submit
+				</button>
+				<p>
+					Already a user?{' '}
+					<Link
+						to="/login"
+						className="member-btn">
+						Login
+					</Link>
+					{/* Must use a tag to create links to external sites */}
+				</p>
+			</form>
+		</Wrapper>
 	);
 };
 export default RegisterPage;
