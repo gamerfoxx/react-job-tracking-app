920869798311cc9ab40dbed9d898b039a203f20f
gamerfoxx <admin@josephludbrook.com>
2023-08-29 12:30:57 -0400

    update navbar with icon

--- src/components/menus/Navbar.jsx
+++ src/components/menus/Navbar.jsx
@@ -1,7 +1,15 @@
 import Wrapper from '../../assets/wrappers/Navbar';
+import { FaHome } from 'react-icons/fa';
 
 function Navbar() {
-	return <Wrapper>Navbar</Wrapper>;
+	return (
+		<div>
+			<h2>
+				<FaHome />
+			</h2>
+			<Wrapper>Navbar</Wrapper>;
+		</div>
+	);
 }
 
 export default Navbar;
